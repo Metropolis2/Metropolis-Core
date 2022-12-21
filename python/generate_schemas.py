c5f12ffbea7de031656cd413dc823f928e08a33b
LucasJavaudin <lucas@lucasjavaudin.com>
2022-12-21 16:58:59 +0100

    Improve `publish` GitHub Action
    - Directories docs/ and examples/ are added to the zip
    - The JSON Schemas are generated and added to the zip

--- python/generate_schemas.py
+++ python/generate_schemas.py
@@ -2,55 +2,69 @@ from json_schema_for_humans.generate import generate_from_filename
 from json_schema_for_humans.generation_configuration import GenerationConfiguration
 
 config = GenerationConfiguration(
-    expand_buttons = True,
-    template_name = "md",
-    link_to_reused_ref = False,
-    markdown_options = {
-        "fenced-code-blocks": {
-            "cssclass": "highlight jumbotron"
-        },
+    expand_buttons=True,
+    template_name="md",
+    link_to_reused_ref=False,
+    markdown_options={
+        "fenced-code-blocks": {"cssclass": "highlight jumbotron"},
         "tables": None,
         "break-on-newline": True,
-        "cuddled-lists": True
+        "cuddled-lists": True,
     },
-    template_md_options = {
-        "badge_as_image": True,
-        "show_heading_numbers": False
-    },
-    with_footer = False,
+    template_md_options={"badge_as_image": True, "show_heading_numbers": False},
+    with_footer=False,
 )
 
-generate_from_filename("schema-agents.json", "schema-agents.md", config=config)
-generate_from_filename("schema-roadnetwork.json", "schema-roadnetwork.md", config=config)
-generate_from_filename("schema-parameters.json", "schema-parameters.md", config=config)
-generate_from_filename("schema-aggregate-results.json", "schema-aggregate-results.md", config=config)
-generate_from_filename("schema-agent-results.json", "schema-agent-results.md", config=config)
-generate_from_filename("schema-skim-results.json", "schema-skim-results.md", config=config)
-generate_from_filename("schema-weight-results.json", "schema-weight-results.md", config=config)
+generate_from_filename("schemas/schema-agents.json", "schemas/schema-agents.md", config=config)
+generate_from_filename(
+    "schemas/schema-roadnetwork.json", "schemas/schema-roadnetwork.md", config=config
+)
+generate_from_filename(
+    "schemas/schema-parameters.json", "schemas/schema-parameters.md", config=config
+)
+generate_from_filename(
+    "schemas/schema-aggregate-results.json", "schemas/schema-aggregate-results.md", config=config
+)
+generate_from_filename(
+    "schemas/schema-agent-results.json", "schemas/schema-agent-results.md", config=config
+)
+generate_from_filename(
+    "schemas/schema-skim-results.json", "schemas/schema-skim-results.md", config=config
+)
+generate_from_filename(
+    "schemas/schema-weight-results.json", "schemas/schema-weight-results.md", config=config
+)
 
 config = GenerationConfiguration(
-    expand_buttons = True,
-    template_name = "js",
-    link_to_reused_ref = False,
-    markdown_options = {
-        "fenced-code-blocks": {
-            "cssclass": "highlight jumbotron"
-        },
+    expand_buttons=True,
+    template_name="js",
+    link_to_reused_ref=False,
+    markdown_options={
+        "fenced-code-blocks": {"cssclass": "highlight jumbotron"},
         "tables": None,
         "break-on-newline": True,
-        "cuddled-lists": True
+        "cuddled-lists": True,
     },
-    template_md_options = {
-        "badge_as_image": True,
-        "show_heading_numbers": False
-    },
-    with_footer = False,
+    template_md_options={"badge_as_image": True, "show_heading_numbers": False},
+    with_footer=False,
 )
 
-generate_from_filename("schema-agents.json", "schema-agents.html", config=config)
-generate_from_filename("schema-roadnetwork.json", "schema-roadnetwork.html", config=config)
-generate_from_filename("schema-parameters.json", "schema-parameters.html", config=config)
-generate_from_filename("schema-aggregate-results.json", "schema-aggregate-results.html", config=config)
-generate_from_filename("schema-agent-results.json", "schema-agent-results.html", config=config)
-generate_from_filename("schema-skim-results.json", "schema-skim-results.html", config=config)
-generate_from_filename("schema-weight-results.json", "schema-weight-results.html", config=config)
+generate_from_filename("schemas/schema-agents.json", "schemas/schema-agents.html", config=config)
+generate_from_filename(
+    "schemas/schema-roadnetwork.json", "schemas/schema-roadnetwork.html", config=config
+)
+generate_from_filename(
+    "schemas/schema-parameters.json", "schemas/schema-parameters.html", config=config
+)
+generate_from_filename(
+    "schemas/schema-aggregate-results.json", "schemas/schema-aggregate-results.html", config=config
+)
+generate_from_filename(
+    "schemas/schema-agent-results.json", "schemas/schema-agent-results.html", config=config
+)
+generate_from_filename(
+    "schemas/schema-skim-results.json", "schemas/schema-skim-results.html", config=config
+)
+generate_from_filename(
+    "schemas/schema-weight-results.json", "schemas/schema-weight-results.html", config=config
+)
