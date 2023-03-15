986de551af3e14ca9c7d701db80235c6b9f3c022
LucasJavaudin <lucas@lucasjavaudin.com>
2023-03-15 16:36:51 +0100

    Fix typo in a debug assertion

--- tch/src/contraction_hierarchies.rs
+++ tch/src/contraction_hierarchies.rs
@@ -243,7 +243,7 @@ impl<T: TTFNum> HierarchyOverlay<T> {
         if cfg!(debug_assertions) {
             // Check that there is no loop in the path.
             let n = unpacked_path.iter().collect::<HashSet<_>>().len();
-            assert_eq!(n, path.len(), "Invalid path: {unpacked_path:?}");
+            assert_eq!(n, unpacked_path.len(), "Invalid path: {unpacked_path:?}");
         }
         Ok(unpacked_path)
     }
